a49f454a59d11a2f08d0b916f0325228351a4f04
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2023-01-06 14:57:10 -0500

    remove old ga footer

--- components/footer.vue
+++ components/footer.vue
@@ -66,17 +66,7 @@
 			</p></div>
 			<div class="credit" data-embed="Footer: credits right area"><p>Exhibition Site by <a href="/">Byte</a></p></div>
 		</div>
-		<!-- Google Tag Manager -->
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-NCJ6JSN');</script>
-		<!-- End Google Tag Manager -->
-		<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NCJ6JSN"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<!-- End Google Tag Manager (noscript) -->
+		
 	</footer>
 
 </template>

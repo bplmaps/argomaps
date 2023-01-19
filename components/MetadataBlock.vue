f76c5595d685f74e62d60a9023d5d5ce0bfb81f1
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2023-01-19 10:53:13 -0500

    implement new collection layout; add tailwind

---
+++ components/MetadataBlock.vue
@@ -0,0 +1,111 @@
+<template>
+ <table class="meta-information">
+					<tr v-for="role, role_key in metadata.name_role_tsim" :key="role_key">
+						<th>{{ role }}</th>
+						<td>
+							<p>{{ metadata.name_tsim[role_key] }}</p>
+						</td>
+					</tr>
+					<tr>
+						<th>Date</th>
+						<td>
+							<p v-for="date in metadata.date_tsim">{{ date }}</p>
+						</td>
+					</tr>
+					<tr v-if="metadata.note_resp_tsim">
+						<th>Name on Item</th>
+						<td>
+							<p>{{ metadata.note_resp_tsim.join("; ") }}</p>
+						</td>
+					</tr>
+					<tr v-if="metadata.publisher_tsi">
+						<th>Publisher</th>
+						<td>
+							<p>{{ metadata.publisher_tsi }}</p>
+						</td>
+					</tr>
+					<tr>
+						<th>Scale</th>
+						<td>
+							<p>{{ metadata.scale_tsim[0] }}</p>
+						</td>
+					</tr>
+					<tr>
+						<th>Language</th>
+						<td>
+							<p>{{ metadata.lang_term_ssim.join("; ") }}</p>
+						</td>
+					</tr>
+
+					<tr v-if="metadata.identifier_local_other_tsim">
+						<th>Identifier</th>
+						<td>
+							<p>{{ metadata.identifier_local_other_tsim.join('; ') }}</p>
+						</td>
+					</tr>
+					<tr>
+						<th>Location</th>
+						<td>
+							<p>{{ metadata.physical_location_ssim.join("; ") }}</p>
+						</td>
+					</tr>
+					<tr>
+						<th>Collection (local)</th>
+						<td>
+							<p>{{ metadata.related_item_host_ssim.join("; ") }}</p>
+						</td>
+					</tr>
+					<tr>
+						<th>Subjects</th>
+						<td>
+							<ul>
+								<li v-for="subject in metadata.subject_topic_tsim">
+								{{ subject }}
+							</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<th>Places</th>
+						<td>
+							<ul>
+								<li v-for="place in metadata.subject_geographic_sim">
+								{{ place }}
+							</li>
+							</ul>
+						</td>
+					</tr>
+					<tr>
+						<th>Extent</th>
+						<td>
+							<p>{{ metadata.extent_tsi }}</p>
+						</td>
+					</tr>
+					<tr>
+						<th>Terms and License</th>
+						<td>
+								<ul>
+									<li>{{ metadata.rights_ss }}</li>
+									<li>{{ metadata.license_ss }}</li>
+								</ul>
+						</td>
+					</tr>
+					<tr v-if="metadata.note_tsim">
+						<th>Notes</th>
+						<td>
+							<ul>
+								<li v-for="note in metadata.note_tsim">
+								{{ note }}</li>
+							</ul>
+						</td>
+					</tr>
+				</table>
+</template>
+
+<script>
+export default {
+	props: {
+		metadata: Object
+	}
+}
+</script>
